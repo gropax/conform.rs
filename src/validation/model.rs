1237fbcdd7cdc6e69a34881c46991f6c9dae21e5
Maxime Laudrin <maxime.laudrin@protonmail.com>
2025-11-26 11:33:22 +0100

    Add FlattenErrors trait

--- src/validation/model.rs
+++ src/validation/model.rs
@@ -5,16 +5,32 @@ use std::collections::HashSet;
 use std::path::PathBuf;
 use url::Url;
 
-#[derive(Debug)]
+pub trait FlattenErrors {
+    fn flatten(&self) -> Vec<ValidationError>;
+}
+
+#[derive(Debug, Clone)]
 pub struct ValidationError {
     pub message: String,
     pub span: document::Span,
+}
+
+impl FlattenErrors for ValidationError {
+    fn flatten(&self) -> Vec<ValidationError> {
+        vec![self.clone()]
+    }
 }
 
 #[derive(Debug)]
 pub struct ValueError {
     pub value_index: usize,
     pub errors: Vec<ValidationError>,
+}
+
+impl FlattenErrors for ValueError {
+    fn flatten(&self) -> Vec<ValidationError> {
+        self.errors.iter().flat_map(|e| e.flatten()).collect()
+    }
 }
 
 #[derive(Debug)]
@@ -24,11 +40,33 @@ pub struct FieldError {
     pub values: Vec<ValueError>,
 }
 
+impl FlattenErrors for FieldError {
+    fn flatten(&self) -> Vec<ValidationError> {
+        let mut out: Vec<ValidationError> = Vec::new();
+
+        out.extend(self.errors.iter().flat_map(|e| e.flatten()));
+        out.extend(self.values.iter().flat_map(|e| e.flatten()));
+
+        out
+    }
+}
+
 #[derive(Debug)]
 pub struct DocumentError {
     pub file_path: PathBuf,
     pub errors: Vec<ValidationError>,
     pub fields: Vec<FieldError>,
+}
+
+impl FlattenErrors for DocumentError {
+    fn flatten(&self) -> Vec<ValidationError> {
+        let mut out: Vec<ValidationError> = Vec::new();
+
+        out.extend(self.errors.iter().flat_map(|e| e.flatten()));
+        out.extend(self.fields.iter().flat_map(|e| e.flatten()));
+
+        out
+    }
 }
 
 trait ValidateNumber {
