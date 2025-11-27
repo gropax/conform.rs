fe77c9dba50e26409aa9a506306635589d406873
Maxime Laudrin <maxime.laudrin@protonmail.com>
2025-11-27 13:16:07 +0100

    Support validation of .md files

--- examples/document.md
+++ examples/document.md
@@ -1,10 +1,11 @@
 ---
-title: "Moonlight serenade"
+# Missing title
 authors:
   - John Robertson
   - Robert Johnson
 cover: "https://www.albums.com/covers/moonlight-serenade"
+duration: "34:21"  # Unknown field
 ---
-# Moonlight serenade
+# ???
 
 This is my favorite album ever!!
--- src/document/io.rs
+++ src/document/io.rs
@@ -30,40 +30,14 @@ pub enum LoadError {
     Document(#[from] DocumentError),
 }
 
-//pub fn parse_json(input: &str, file_path: &PathBuf) -> Result<Document, LoadError> {
-//    let json: JsonValue = serde_json::from_str(input)?;
-//    let document = document(json, file_path)?;
-//    Ok(document)
-//}
-
-//pub fn parse_yaml(input: &str, file_path: &PathBuf) -> Result<Document, LoadError> {
-//    let yaml: YamlValue = serde_yaml::from_str(input)?;
-//    let json: JsonValue = serde_json::to_value(yaml)?;
-//    let document = document(json, file_path)?;
-//    Ok(document)
-//}
-
-fn extract_frontmatter(input: &str) -> Option<&str> {
-    let rest = input.strip_prefix("---\n")?;
-    let end = rest.find("\n---\n")?;
-    let (front, _) = rest.split_at(end);
-    Some(front)
-}
-
-//pub fn parse_markdown(input: &str, file_path: &PathBuf) -> Result<Document, LoadError> {
-//    let frontmatter = extract_frontmatter(input).ok_or(LoadError::Markdown)?;
-//    parse_yaml(frontmatter, file_path)
-//}
-
 pub fn parse(file_path: impl Into<PathBuf>) -> Result<Document, LoadError> {
     let path = file_path.into();
     let input = fs::read_to_string(&path)?;
-    let file = path.to_string_lossy().into_owned();
 
     match path.extension().and_then(|ext| ext.to_str()) {
         //Some("json") => parse_json(&input, &path),
         Some("yaml") | Some("yml") => parse_yaml(&input),
-        //Some("md") | Some("markdown") => parse_markdown(&input, &path),
+        Some("md") | Some("markdown") => parse_markdown(&input),
 
         other => Err(LoadError::UnsupportedExtension(
             other.unwrap_or("").to_string(),
@@ -73,8 +47,8 @@ pub fn parse(file_path: impl Into<PathBuf>) -> Result<Document, LoadError> {
 
 // YAML
 
-fn yaml_to_scalar(v: &MarkedYaml) -> Option<Scalar> {
-    let span = yaml_to_span(v);
+fn yaml_to_scalar(v: &MarkedYaml, line_offset: usize) -> Option<Scalar> {
+    let span = yaml_to_span(v, line_offset);
 
     match &v.data {
         YamlData::Value(s) => match s {
@@ -88,10 +62,10 @@ fn yaml_to_scalar(v: &MarkedYaml) -> Option<Scalar> {
     }
 }
 
-fn yaml_to_value(v: &MarkedYaml) -> Value {
-    let span = yaml_to_span(v);
+fn yaml_to_value(v: &MarkedYaml, line_offset: usize) -> Value {
+    let span = yaml_to_span(v, line_offset);
 
-    if let Some(scalar) = yaml_to_scalar(v) {
+    if let Some(scalar) = yaml_to_scalar(v, line_offset) {
         Value::Single { span, value: scalar }
     } else {
         match &v.data {
@@ -99,7 +73,7 @@ fn yaml_to_value(v: &MarkedYaml) -> Value {
                 let mut scalars = vec![];
 
                 for item in arr {
-                    match yaml_to_scalar(item) {
+                    match yaml_to_scalar(item, line_offset) {
                         Some(s) => scalars.push(s),
                         None => return Value::Invalid { span },
                     }
@@ -112,7 +86,7 @@ fn yaml_to_value(v: &MarkedYaml) -> Value {
     }
 }
 
-fn yaml_to_key(k: &MarkedYaml) -> Result<FieldKey, DocumentError> {
+fn yaml_to_key(k: &MarkedYaml, line_offset: usize) -> Result<FieldKey, DocumentError> {
     let name = match k.data.as_str() {
         Some(s) => s,
         None => return Err(DocumentError::NonStringKey),
@@ -120,20 +94,20 @@ fn yaml_to_key(k: &MarkedYaml) -> Result<FieldKey, DocumentError> {
 
     let key = FieldKey {
         name: name.to_string(),
-        span: yaml_to_span(k),
+        span: yaml_to_span(k, line_offset),
     };
 
     Ok(key)
 }
 
-fn yaml_to_span(v: &MarkedYaml) -> Span {
+fn yaml_to_span(v: &MarkedYaml, line_offset: usize) -> Span {
     Span {
-        line: v.span.start.line(),
+        line: v.span.start.line() + line_offset,
         column: v.span.start.col(),
     }
 }
 
-pub fn yaml_to_document(root: &MarkedYaml) -> Result<Document, DocumentError> {
+pub fn yaml_to_document(root: &MarkedYaml, line_offset: usize) -> Result<Document, DocumentError> {
     let object = match root.data.as_mapping() {
         Some(obj) => obj,
         None => return Err(DocumentError::RootNotObject)
@@ -142,9 +116,8 @@ pub fn yaml_to_document(root: &MarkedYaml) -> Result<Document, DocumentError> {
     let mut fields = HashMap::new();
 
     for (k, v) in object.iter() {
-        let key = yaml_to_key(k)?;
-        let value = yaml_to_value(v);
-
+        let key = yaml_to_key(k, line_offset)?;
+        let value = yaml_to_value(v, line_offset);
         fields.insert(
             key.name.to_string(),
             Field {
@@ -156,14 +129,32 @@ pub fn yaml_to_document(root: &MarkedYaml) -> Result<Document, DocumentError> {
 
     Ok(Document {
         fields,
-        span: yaml_to_span(root),
+        span: yaml_to_span(root, line_offset),
     })
 }
 
-pub fn parse_yaml(input: &str) -> Result<Document, LoadError> {
+pub fn parse_yaml_with_offset(input: &str, line_offset: usize) -> Result<Document, LoadError> {
     let yaml_docs = MarkedYaml::load_from_str(input)?;
     // Error in multiple documents
     let yaml_doc = &yaml_docs[0];
-    let document = yaml_to_document(yaml_doc)?;
+    let document = yaml_to_document(yaml_doc, line_offset)?;
     Ok(document)
+}
+
+pub fn parse_yaml(input: &str) -> Result<Document, LoadError> {
+    parse_yaml_with_offset(input, 0)
+}
+
+// Markdown with YAML frontmatter
+
+fn extract_frontmatter(input: &str) -> Option<&str> {
+    let rest = input.strip_prefix("---\n")?;
+    let end = rest.find("\n---\n")?;
+    let (front, _) = rest.split_at(end);
+    Some(front)
+}
+
+pub fn parse_markdown(input: &str) -> Result<Document, LoadError> {
+    let frontmatter = extract_frontmatter(input).ok_or(LoadError::Markdown)?;
+    parse_yaml_with_offset(frontmatter, 1)
 }
